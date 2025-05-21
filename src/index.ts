98fe5ceee247dcc2f65cb42c0a9f6f7672b1aeef
XRandolphX <randolphfrp@gmail.com>
2025-05-21 13:51:46 -0500

    feat: set up Express server with product and order routes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,25 @@ import cors from "cors";
 import dotenv from "dotenv";
 import productsRoutes from "./routes/products";
 import ordersRoutes from "./routes/orders";
+// import type { Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", productsRoutes);
+app.use("/api/orders", ordersRoutes);
+
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(` Server running on http://localhost: ${PORT}`);
+});
+
+
